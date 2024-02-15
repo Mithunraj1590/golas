279200b2a88e3a9d011669beb3455f811b2bd293
Amal ks <amalks8001@gmail.com>
2024-02-15 23:30:35 +0530

    animation hook craeted

--- src/widgets/HomeServices/index.js
+++ src/widgets/HomeServices/index.js
@@ -2,87 +2,15 @@
 import React from 'react'
 import Style from "./HomeService.module.scss"
 import parse from "html-react-parser";
-import gsap from 'gsap';
 import { useRef, useEffect } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
+import useAnimation from './useAnimation';
 
 const HomeService = ({ data }) => {
-    const serviceWrp = useRef();
-    // const page = gsap.utils.toArray("." + Style.service_wrap);
+    const { Animcontainer } = useAnimation()
 
-    useEffect(() => {
-
-        const ctx = gsap.context((self) => {
-            const el = self.selector(`.wrap`);
-            const tl = gsap.timeline({
-                scrollTrigger: {
-                    trigger: serviceWrp.current,
-          
-                    pin: true,
-                    start: "top top",
-                    end: "bottom -100%",
-                    scrub: true,
-                    ease: "none",
-                    stagger: 1,
-                    
-
-                },
-            })
-
-            function setCard() {
-
-
-                el.forEach((elm, index) => {
-                    if (index > 0) {
-                        gsap.set(elm, {
-                            opacity: 1 - (index * 0.1),
-                            yPercent: index * 110,
-                            willChange: 'transform, opacity',
-                            scale: .8,
-                            duration: 10,
-
-                        })
-
-                    }
-                    console.log(index);
-                });
-            }
-            setCard()
-
-
-
-            el.forEach((elm, index) => {
-
-                gsap.set(elm, { zIndex: index })
-                if (index > 0) {
-                    tl.to(elm, {
-                        opacity: 1,
-                        force3D: true,
-                        yPercent: index * 0,
-                        // zIndex:index+1,
-                        willChange: 'transform, opacity',
-                        duration: 10,
-                        scale: 1
-                    })
-                    tl.to(el[index - 1], {
-                        opacity: 0,
-                        
-                        willChange: 'opacity',
-                        duration: 20,
-                    }, "<")
-                }
-            });
-
-
-
-
-        }, serviceWrp)
-        return () => ctx.revert();
-    }, []);
 
     return (
-        <section className={Style.homeservice} ref={serviceWrp}>
+        <section className={Style.homeservice} ref={Animcontainer}>
             {data?.services?.map((data, i) => {
                 return (
                     <div className={`${Style.service_wrap} wrap`} key={i} >
---
+++ src/widgets/HomeServices/useAnimation.js
@@ -0,0 +1,80 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from 'react';
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+const useAnimation = () => {
+    const Animcontainer = useRef()
+    console.log(Animcontainer.current);
+    // useGSAP((context) => {
+
+    //     // const el = self.selector(`.wrap`);
+    //     // const tl = gsap.timeline({
+    //     //     scrollTrigger: {
+    //     //         trigger: serviceWrp.current,
+
+    //     //         pin: true,
+    //     //         start: "top top",
+    //     //         end: "bottom -100%",
+    //     //         scrub: true,
+    //     //         ease: "none",
+    //     //         stagger: 1,
+
+
+    //     //     },
+    //     // })
+
+    //     // function setCard() {
+
+
+    //     //     el.forEach((elm, index) => {
+    //     //         if (index > 0) {
+    //     //             gsap.set(elm, {
+    //     //                 opacity: 1 - (index * 0.1),
+    //     //                 yPercent: index * 110,
+    //     //                 willChange: 'transform, opacity',
+    //     //                 scale: .8,
+    //     //                 duration: 10,
+
+    //     //             })
+
+    //     //         }
+    //     //         console.log(index);
+    //     //     });
+    //     // }
+    //     // setCard()
+
+
+
+    //     // el.forEach((elm, index) => {
+
+    //     //     gsap.set(elm, { zIndex: index })
+    //     //     if (index > 0) {
+    //     //         tl.to(elm, {
+    //     //             opacity: 1,
+    //     //             force3D: true,
+    //     //             yPercent: index * 0,
+    //     //             // zIndex:index+1,
+    //     //             willChange: 'transform, opacity',
+    //     //             duration: 10,
+    //     //             scale: 1
+    //     //         })
+    //     //         tl.to(el[index - 1], {
+    //     //             opacity: 0,
+
+    //     //             willChange: 'opacity',
+    //     //             duration: 20,
+    //     //         }, "<")
+    //     //     }
+    //     // });
+
+
+    // }, []);
+
+    return { Animcontainer }
+}
+
+export default useAnimation;
