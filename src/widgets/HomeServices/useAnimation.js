c9173e91d66032fa07e3dcfaee26bf4ef7a592c7
Amal ks <amalks8001@gmail.com>
2024-02-18 14:19:18 +0530

    desk card stack compltd

--- src/widgets/HomeServices/useAnimation.js
+++ src/widgets/HomeServices/useAnimation.js
@@ -8,70 +8,66 @@ gsap.registerPlugin(ScrollTrigger);
 
 const useAnimation = () => {
     const Animcontainer = useRef()
-    // useGSAP((context) => {
 
-    //     // const el = self.selector(`.wrap`);
-    //     // const tl = gsap.timeline({
-    //     //     scrollTrigger: {
-    //     //         trigger: serviceWrp.current,
+    useGSAP((context) => {
 
-    //     //         pin: true,
-    //     //         start: "top top",
-    //     //         end: "bottom -100%",
-    //     //         scrub: true,
-    //     //         ease: "none",
-    //     //         stagger: 1,
+        const el = context.selector(`.wrap`);
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: Animcontainer.current,
+                pin: true,
+                start: "top top",
+                end: "bottom -500px",
+                scrub: 3,
+                stagger: 1,
+                markers: true,
+            },
+        })
 
+        function setCard() {
 
-    //     //     },
-    //     // })
+            el.forEach((elm, index) => {
+                if (index > 0) {
+                    gsap.set(elm, {
+                        opacity: 1 - (index * 0.1),
+                        yPercent: index * 110,
+                        clipPath: "inset(20% 20% 0 20%)"
+                        // scale: .8,
+                        // duration:5,
+                    })
 
-    //     // function setCard() {
-
-
-    //     //     el.forEach((elm, index) => {
-    //     //         if (index > 0) {
-    //     //             gsap.set(elm, {
-    //     //                 opacity: 1 - (index * 0.1),
-    //     //                 yPercent: index * 110,
-    //     //                 willChange: 'transform, opacity',
-    //     //                 scale: .8,
-    //     //                 duration: 10,
-
-    //     //             })
-
-    //     //         }
-    //     //         console.log(index);
-    //     //     });
-    //     // }
-    //     // setCard()
+                }
+                console.log(index);
+            });
+        }
+        setCard()
 
 
 
-    //     // el.forEach((elm, index) => {
+        el.forEach((elm, index) => {
 
-    //     //     gsap.set(elm, { zIndex: index })
-    //     //     if (index > 0) {
-    //     //         tl.to(elm, {
-    //     //             opacity: 1,
-    //     //             force3D: true,
-    //     //             yPercent: index * 0,
-    //     //             // zIndex:index+1,
-    //     //             willChange: 'transform, opacity',
-    //     //             duration: 10,
-    //     //             scale: 1
-    //     //         })
-    //     //         tl.to(el[index - 1], {
-    //     //             opacity: 0,
+            gsap.set(elm, { zIndex: index })
+            if (index > 0) {
+                tl.to(elm, {
+                    opacity: 1,
+                    force3D: true,
+                    yPercent: index * 0,
+                    // zIndex:index+1,
+                    willChange: 'transform, opacity',
+                    duration: 10,
+                    clipPath: "inset(0%)"
 
-    //     //             willChange: 'opacity',
-    //     //             duration: 20,
-    //     //         }, "<")
-    //     //     }
-    //     // });
+                })
+                tl.to(el[index - 1], {
+                    opacity: 0,
+                    willChange: 'opacity',
+                    duration: 20,
+                }, "<")
+            }
+        });
 
 
-    // }, []);
+    }, { scope: Animcontainer });
 
     return { Animcontainer }
 }
