a2c4c7615d131a3de50110a6fc1f82d61b25464e
Amal ks <amalks8001@gmail.com>
2024-03-20 23:25:38 +0530

    conflct fxed

--- src/widgets/HomeServices/index.js
+++ src/widgets/HomeServices/index.js
@@ -2,43 +2,6 @@
 import React from "react";
 import Style from "./HomeService.module.scss";
 import parse from "html-react-parser";
-<<<<<<< HEAD
-import useAnimation from './useAnimation';
-
-const HomeService = ({ data }) => {
-    const { Animcontainer } = useAnimation()
-    return (
-        <section className={Style.homeservice} ref={Animcontainer}>
-            {data?.services?.map((data, i) => {
-                return (
-                    <div className={`${Style.service_wrap} wrap`} key={i} >
-                        <div className={Style.outer_wrap}>
-
-
-                            <div className={Style.img_vdo_wrap}>
-                                <figure>
-                                    <img src={data?.image} alt="service" />
-                                </figure>
-                            </div>
-                            <div className={Style.service_content}>
-                                <div className="container">
-                                    <h2 className={`h2 ${Style.title}`}>{data?.title}</h2>
-                                    <div className={Style.service}>
-                                        {parse(data?.service)}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
-
-        </section>
-    )
-}
-
-export default HomeService
-=======
 import gsap from "gsap";
 import { useRef, useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -174,4 +137,3 @@ const HomeService = ({ data }) => {
 };
 
 export default HomeService;
->>>>>>> main
